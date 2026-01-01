4e15a3ca89cb27d9803c4b4f55e27ed7da165c8b
Kevinuyeh <isrealkevin929@gmail.com>
2026-01-01 02:46:58 -0500

    new commit

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -7,18 +7,18 @@ function Socials() {
     <div className='w-full bg-mondo p-4'>
           <div className='max-w-[1240px] mx-auto grid grid-cols-2 place-items-center gap-8'>
       <div className='flex items-center justify-center'>
-                <FaInstagram className='ml-3 text-cream text-3xl md:text-5xl' />
+                <FaInstagram className='text-cream text-3xl md:text-5xl' />
                 <div className='flex flex-col text-center'>
-                <p className='text-cream font-bold mx-5'>#Brusselscoffee</p>
-                <p className='text-cream text-sm mx-5'>official_steam_coffee</p>
+                <p className='text-cream font-bold md:mx-5'>#Brusselscoffee</p>
+                <p className='text-cream text-sm md:mx-5'>official_steam_coffee</p>
                 </div>
             </div>
 
             <div className='flex items-center justify-center'>
                 <FaFacebook className='text-cream text-3xl md:text-5xl'/>
                 <div className='flex flex-col text-center'>
-                <p className='text-cream mx-5 font-bold'>#Brusselscoffee</p>
-                <p className='text-cream text-sm mx-5'>official_steam_coffee</p>
+                <p className='text-cream md:mx-5 font-bold'>#Brusselscoffee</p>
+                <p className='text-cream text-sm md:mx-5'>official_steam_coffee</p>
                 </div>
             </div>
             
